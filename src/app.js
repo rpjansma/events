f85c79bc743428cf4cf28887aa0a1d8ec9f881f0
Raul <raul.paes@fatec.sp.gov.br>
2021-05-12 11:11:28 -0300

    Another config to help Heroku to find my main directory.

--- src/app.js
+++ src/app.js
@@ -22,6 +22,9 @@ const usersRoutes = require("./routes/usersRoute");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
 app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
