01fa9820b75966ebe76e0807e32bc06372035dc8
Raul Jansma Paes <rpjansma@gmail.com>
2021-11-20 17:29:26 -0300

    Small fix

--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -6,8 +6,8 @@ const router = express.Router();
 //Test route
 const route = router.get('/', (req, res, next) => {
   res.status(200).send({
-    title: "Calendar API",
-    version: "0.0.2"
+    title: "Events API",
+    version: "1.0.0"
   });
 });
 
